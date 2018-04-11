0f3446e9225948821bf397f4fec714516eb56870
kuroironeko <36471934+kuroironeko@users.noreply.github.com>
2018-04-11 14:12:15 +0000

    Fix link of top page

---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "users/show", type: :view do
+  before(:each) do
+    @user = assign(:user, FactoryBot.create(:user))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/MyName/)
+    expect(rendered).to match(/myemail@myemail.com/)
+    expect(rendered).to match(/MyPassword/)
+  end
+end
