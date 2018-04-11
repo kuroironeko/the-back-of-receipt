abfb6e588975e4cc54f803c17c1f9ece59855876
kuroironeko <36471934+kuroironeko@users.noreply.github.com>
2018-04-11 14:26:03 +0000

    Fix_usercotroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show, :edit]
   # GET /products/1
   def show
   end
@@ -7,5 +7,11 @@ class UsersController < ApplicationController
   # GET /users/1/edit
   def edit
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
 
 end
