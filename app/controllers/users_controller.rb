54794e313892a922386373863f64bf3ce5224759
kuroironeko <36471934+kuroironeko@users.noreply.github.com>
2018-04-12 09:02:12 +0000

    Tiny fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,9 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: [:show, :edit, :update]
 
-  # GET /users/1
   def show
   end
 
-  # GET /users/1/edit
   def edit
   end
 
