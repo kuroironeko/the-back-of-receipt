f795b80d58937c84f6a07212c8a16a4a3cf0791c
kuroironeko <36471934+kuroironeko@users.noreply.github.com>
2018-05-17 08:30:19 +0000

    Change http method for "accept" and "join"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :groups do
     member do
-      patch 'join'
+      post 'join'
     end
     resources :products
     resources :products_indices
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   devise_for :users, path: 'accounts'
   resources :users do
     member do
-      patch 'accept'
+      post 'accept'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
