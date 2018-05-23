11ce4c68978024df99e97424dc387d81789a7d66
kuroironeko <36471934+kuroironeko@users.noreply.github.com>
2018-05-23 08:37:39 +0000

    Create templates of mails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,9 @@
-# class for whole app
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: ENV['EMAIL']
   layout 'mailer'
+
+  def requests_mail(user)
+    @products_indices = ProductsIndex.where(group_id: user.group_id)
+    mail(to: user.email, subject: t(:updated_requests))
+  end
 end
