b5e45101a8962664fde5752b653dd66d6cfd4f29
kuroironeko <36471934+kuroironeko@users.noreply.github.com>
2018-09-16 10:39:05 +0000

    Add test code to linebot controller

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,2 +1,36 @@
 class LinebotController < ApplicationController
+    require 'line/bot'
+
+    def client
+        @client ||= Line::Bot::Client.new { |config|
+          config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+          config.channel_token = ENV["LINE_CHANNELTOKEN"]
+        }
+      end
+
+    def callback
+        body = request.body.read
+
+        signature = request.env['HTTP_X_LINE_SIGNATURE']
+        unless client.validate_signature(body, signature)
+            error 400 do 'Bad Request' end
+        end
+
+        events = client.parse_events_from(body)
+
+        events.each { |event|
+            case event
+            when Line::Bot::Event::Message
+                case event.type
+                when Line::Bot::Event::Message::Text
+                    message = {
+                        type:'text',
+                        text: event.message['text']
+                    }
+                    client.reply_message(event['replyToken'], message)
+                end
+            end
+        }
+        "OK"
+    end
 end
